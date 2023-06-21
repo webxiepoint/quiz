0b8d53feb35093c2b2f56d525e2cfa42281de8cf
webxiepoint <109460927+webxiepoint@users.noreply.github.com>
2023-06-21 19:28:37 +0530

    Morph-nav.js

--- Morph-nav.js
+++ Morph-nav.js
@@ -2,9 +2,9 @@
 jQuery(document).ready(function($) {
   var alterClass = function() {
     var ww = document.body.clientWidth;
-    if (ww < 1206) {
+    if (ww > 1206) {
       $('#header-saka').removeClass('saka-mega-nav--mobile');
-    } else if (ww >= 1207) {
+    } else if (ww <= 1207) {
       $('#header-saka').addClass('saka-mega-nav--mobile');
     };
   };
